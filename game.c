0432a99e1e74c0c534b65773373b4af3d15635ef
Emelie Söderlund <emeso@kth.se>
2025-11-25 10:24:58 +0100

    add interrupts

--- game.c
+++ game.c
@@ -1,5 +1,7 @@
 #include "settings.c"
 
+GameState gs;
+int game_running = 1;
 
 typedef struct {
     int x;        // left side of the pipe
@@ -7,7 +9,7 @@ typedef struct {
     int width;    // usually 3–5 chars wide
 } Pipe;
 
-/// Game state struct
+/// ---------Game state struct------------------
 typedef struct {
     int birdY;
     int velocity;
@@ -27,7 +29,7 @@ struct rect {
 };
 
 
-// init functions
+// --------------init functions--------------
 void initGame(GameState *gs) {
     gs->birdY = HEIGHT / 2;
     gs->velocity = 0;
@@ -42,21 +44,85 @@ void resetPipe(GameState *gs) {
 }
 
 
-/// game
+/// -------------Game-----------
+
+void handle_interrupt(unsigned cause)
+{
+    volatile unsigned int* status = (volatile unsigned int*) 0x04000020;
+
+    if (*status & 1) {
+        *status = 0;
+
+        processInput(&gs);
+        applyGravity(&gs);
+        updatePipes(&gs);
+
+        if (checkCollision(&gs)) {
+            game_running = 0;
+        }
+
+        draw(&gs);
+    }
+}
+
+void initInterruptTimer(void)
+{
+    int timer_base = 0x04000020;
+    int cycles = 3000000-1;  // 30 MHz, 100ms frame
+
+    volatile unsigned short* period_l = (volatile unsigned short*)(timer_base + 0x08);
+    volatile unsigned short* period_h = (volatile unsigned short*)(timer_base + 0x0C);
+
+    *period_l = cycles & 0xFFFF;
+    *period_h = (cycles >> 16) & 0xFFFF;
+
+    volatile int* control =  (volatile int*)(timer_base + 0x04);
+    volatile int* status  = (volatile int*)(timer_base + 0x00);
+
+    *status = 0;     // clear pending
+    *control = 6;    // enable timer + interrupt
+}
+
 applyGravity(GameState *gs){
 
 }
 
 
+int checkCollision(GameState *gs) {
+    int birdX = 5; // bird is always in the middle of the screen
+
+    for (int i = 0; i < MAX_PIPES; i++) {
+        Pipe *p = &gs->pipes[i];
+
+        // compare x-coordinate for pipe and bird
+        if (birdX >= p->x && birdX < p->x + p->width) {
+
+            // top pipe collision
+            if (gs->birdY < p->gapY) return 1;
+
+            // bottom pipe collision
+            if (gs->birdY > p->gapY + PIPE_GAP) return 1;
+        }
+    }
+
+    return 0;
+}
+
+// ----input-----
 processInput(GameState *gs){
+int btn = get_btn();
 
+    if (btn != 0) {
+        gs->velocity = JUMP_STRENGTH;
+    }
 }
 
-checkCollision(GameState *gs){
-
+int get_btn(void){
+  volatile int* button_status = (volatile int*) 0x040000d0;
+  return *button_status;
 }
 
-// Score
+// ------------Score----------
 
 void updateScore(GameState *gs) { // When pipe moves left past the bird
     if (gs->pipeX == 1) {   
@@ -69,13 +135,19 @@ void showScore(GameState *gs) {
 }
 
 
-// main
-while (game is running)
-{
+//  ------------------main----------------
+int main(int argc, char const *argv[]){
+
+    initGame(&gs);
+    initInterruptTimer();
+
+    while (game is running){
     processInput()
     applyGravity()
     updatePipe()
     if (collision) stop
     draw()
     delay()
+    }
 }
+
